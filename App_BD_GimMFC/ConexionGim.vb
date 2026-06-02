a80fa9bc325ac4196681651b7c7318f36016b2b6
myrarimoreno-design <myrarimoreno@gmail.com>
2026-06-02 12:28:19 -0600

    Update ConexionGim.vb
    cambio computadora

--- App_BD_GimMFC/ConexionGim.vb
+++ App_BD_GimMFC/ConexionGim.vb
@@ -1,7 +1,7 @@
 ﻿Module ConexionGim
     'La variable conexion es la que permite conectarnos a nuestro sql 
 
-    Public conexion As New SqlClient.SqlConnection("data source=DESKTOP-M9E0OMK\SQLEXPRESS; initial catalog =Gimnasio; integrated security=SSPI; persist security info = false; trusted_connection = yes;")
+    Public conexion As New SqlClient.SqlConnection("data source=DESKTOP-P1KRNOI\SQLEXPRESS; initial catalog =Gimnasio; integrated security=SSPI; persist security info = false; trusted_connection = yes;")
 
     'Nos permite ejecutar los comandos que tiene que ver con los procedimientos  almacenados de sql 
 
